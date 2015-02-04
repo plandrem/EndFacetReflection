d663f7e046e7e6a8f59aaf96e9c7a75facc330ea
Patrick Landreman <patrick.landreman@gmail.com>
2015-02-03 18:34:06 -0800

    break out tests into discrete file

--- EndFacet.py
+++ EndFacet.py
@@ -210,523 +210,6 @@ def beta_marcuse(n,d,wl=1.,pol='TM',polarity='even',Nmodes=None,plot=False):
 
 	return np.array(Bs)
 
-def test_beta_marcuse():
-
-	target_vals_TE = [0.25781,0.54916,1.21972,1.93825,2.66839,4.13075]
-	target_vals_TM = [0.25207,0.51677,1.12809,1.84210,2.58934,4.08131]
-
-	wl = 1
-
-	n = 1.432
-	k = 2*pi/wl
-
-	kds = np.array([0.25,0.5,1.0,1.5,2.0,3.0])
-	ds = kds/k
-
-	print 'TE'
-	for i,d in enumerate(ds):
-		bTE = beta_marcuse(n,d,pol='TE',wl=wl)[0]
-		print "%.5f, %.5f, %.5f" % (bTE * d, target_vals_TE[i], abs((bTE * d - target_vals_TE[i]) / target_vals_TE[i]))
-
-	print
-	print 'TM'
-
-	for i,d in enumerate(ds):
-		bTM = beta_marcuse(n,d,pol='TM',wl=wl)[0]
-		print "%.5f, %.5f, %.5f" % (bTM * d, target_vals_TM[i], abs((bTM * d - target_vals_TM[i]) / target_vals_TM[i]))
-
-def PrettyPlots():
-
-	n = sqrt(20)
-	
-	incident_mode = 0
-	pol='TE'
-	polarity = 'even'
-
-
-	fname = DATA_PATH + '/Rectangular Resonator/End Facet Reflection Coefs/endFacet_a' + str(incident_mode) + '_' + pol + '_' + polarity + '_' + str(n) + '.txt'
-
-	with open(fname,'r') as f:
-		s = f.read()
-
-	s = s.replace('+-','-')
-	s = s.replace('(','')
-	s = s.replace(')','')
-
-	with open(DATA_PATH + '/endfacet_temp.txt','w') as f:	
-		f.write(s)
-
-
-	data = np.loadtxt(DATA_PATH + '/endfacet_temp.txt', dtype=complex)
-	
-	N = data.shape[0] - 2
-
-	kd = data[0,:]
-	err = data[-1,:]
-
-	idx = np.arange(N) + 1
-
-	fig, ax = plt.subplots(2,sharex=True,figsize=(4.5,7))
-
-	for i in idx:
-		ax[0].plot(kd,  abs(data[i,:])        , color=colors[i-1], lw=2)
-		ax[1].plot(kd, -np.angle(data[i,:])/pi, color=colors[i-1], lw=2)
-
-	ax[1].axhline(0, color='k', ls=':')
-
-	# ax[0].set_xlim(0.6,1.5)
-	ax[0].set_ylim(0,1.1)
-	
-	# ax[1].set_ylim(1/6.,2/3.)
-
-	ax[1].set_xlabel(r'$k_{o}d$')
-
-	ax[0].set_ylabel(r'$|a_{n}|$', fontsize=14)
-	ax[1].set_ylabel(r'$\angle a_{n}$', fontsize=14)
-
-	labels = [r'$a_{%u}$' % (2*j) for j in range(3)]
-	ax[1].legend(labels, loc='lower left')
-
-	ax[0].set_title(pol + ' - ' + r'Source Mode: $a_{%u}$' % 2*incident_mode, fontproperties=prop, fontsize=18)
-
-	plt.tight_layout()
-	plt.show()
-
-def convergence_test_single():
-	'''
-	For a specific value of kd, sweep pmax and pres and plot the result
-	'''
-
-	kd = 2.
-
-	n = sqrt(20)
-
-	res = np.arange(1,15,1) * 1e2
-	p_max = np.arange(5,31,2.5)
-
-	incident_mode = 0
-	pol='TE'
-	polarity = 'even'
-
-	imax = 200
-
-	plt.ion()
-
-	fig, ax = plt.subplots(3,figsize=(10,8),sharex=True,sharey=True)
-	plt.show()
-
-	aos = np.zeros((len(res),len(p_max)),dtype=complex) * complex(np.nan,np.nan)
-	accuracy = np.zeros((len(res),len(p_max)),dtype=complex) * complex(np.nan,np.nan)
-
-	mag = ax[0].imshow(abs(aos), aspect='auto')
-	divmag = make_axes_locatable(ax[0])
-	cmag = divmag.append_axes("right", size="1%", pad=0.05)
-	cbarmag = plt.colorbar(mag,cax=cmag,format='%.2f')
-
-	phase = ax[1].imshow(np.angle(aos)/pi, aspect='auto')
-	divphase = make_axes_locatable(ax[1])
-	cphase = divphase.append_axes("right", size="1%", pad=0.05)
-	cbarphase = plt.colorbar(phase,cax=cphase,format='%.2f')
-
-	error = ax[2].imshow(abs(accuracy), aspect='auto')
-	diverror = make_axes_locatable(ax[2])
-	cerror = diverror.append_axes("right", size="1%", pad=0.05)
-	cbarerror = plt.colorbar(error,cax=cerror,format='%.2f')
-
-	ax[2].set_xlabel(r'Max$\{\rho\}$')
-	ax[1].set_ylabel('res')
-
-
-	for i,res_i in enumerate(res):
-		for j,pmax_j in enumerate(p_max):
-
-			a,dd,bb,acc = SolveForCoefficients(kd,n,
-													pol=pol,
-													polarity=polarity,
-													incident_mode=incident_mode,
-													p_res=res_i,
-													imax=imax,
-													p_max=pmax_j,
-													first_order=False,
-													debug=False
-													)
-
-			try:
-				aos[i,j] = a[0]
-				accuracy[i,j] = acc
-			except TypeError:
-				pass
-
-			# |ao|
-			mag.set_data(abs(aos))
-			mag.set_extent(getExtent(p_max,res))
-			mag.autoscale()
-
-			# phase ao
-			phase.set_data(np.angle(aos)/pi)
-			phase.set_extent(getExtent(p_max,res))
-			phase.autoscale()
-
-			# error
-			error.set_data(abs(accuracy))
-			error.set_extent(getExtent(p_max,res))
-			error.autoscale()
-
-			fig.canvas.draw()
-
-	plt.ioff()
-	plt.show()
-
-def SolveForCoefficients(kd,n,incident_mode=0,pol='TE',polarity='even',
-	p_max=20,p_res=1e3,imax=100,convergence_threshold=1e-5,first_order=False, debug=False, returnMode = None, returnItr = 0,
-	initial_a=None, initial_d=None):
-
-	'''
-	Major code refactor for cleanliness. TE even modes only.
-	'''
-
-	debug = False
-
-	m = incident_mode
-
-	# constants
-	wl = 10. # Set to 10 to match Gelin values for qt
-	k = 2*pi/wl
-	d = kd/k
-	w = c*k
-	eps = n**2
-
-	# Solve slab for all modes
-	N = numModes(n,1,kd)
-	B = beta_marcuse(n,d,wl=wl,pol='TE',polarity='even',Nmodes=N,plot=False)
-
-	print "N:", N
-	print "Betas:", B
-
-	# Make sure the source mode is supported by the current slab
-	if m >= N:
-		print "Mode %u not supported by slab at frequency kd = %.3f" % (m, kd)
-		return np.ones(N) * np.nan, np.ones(N)*np.nan, np.ones(N)*np.nan, np.array(np.nan)
-
-	# Define Wavevectors
-	g  = sqrt(      -k**2 + B**2)						# transverse wavevectors in air for guided modes
-	K  = sqrt(n**2 * k**2 - B**2)						# transverse wavevectors in high-index region for guided modes
-
-	Bc = lambda p: sqrt(k**2 - p**2) * (2 * (sqrt(k**2 - p**2).real > 0) - 1) # Second term is Hamid's correction - not sure why this is necessary yet
-	o  = lambda p: sqrt((n*k)**2 - Bc(p)**2)
-
-	# Mode Amplitude Coefficients
-	P = 1
-
-	A = sqrt(2*w*mu*P / (B*d + B/g))
-
-	Bt = lambda p: sqrt(2*w*mu*P / (pi*abs(Bc(p))))
-	Br = lambda p: sqrt(2*p**2*w*mu*P / (pi*abs(Bc(p))*(p**2*cos(o(p)*d)**2 + o(p)**2*sin(o(p)*d)**2)))
-	Dr = lambda p: 1/2. * exp(-1j*p*d) * (cos(o(p)*d) + 1j*o(p)/p * sin(o(p)*d))
-
-	def Z(p):
-		alpha = 1 if p <= k else 1j
-		return alpha * w * mu / abs(Bc(p))
-		# return abs(Bc(p))
-	Z = np.vectorize(Z,otypes=[np.complex])
-
-	# Define Helper functions for integrals
-
-	G = lambda m,p: 2 * k**2 * (eps-1) * A[m] * Bt(p) * cos(K[m]*d) * (g[m]*cos(p*d) - p*sin(p*d)) / ((K[m]**2 - p**2)*(g[m]**2 + p**2))
-
-	def Ht(qr,p1,p2):
-
-		'''
-		qr is taken in as simply qr(p). To correspond to qr(p') in this 2D matrix formulation, it needs
-		to be reshaped into a 2D matrix, whose values are dependent by row (0th axis), but not by column.
-		'''
-		qr = np.tile(qr,(len(qr),1)).transpose()
-
-		return -qr * (B[m] - Bc(p1)) * k**2 * (eps-1) * Bt(p2) * Br(p1) * (  sin( (o(p1)+p2) *d)/(o(p1)+p2)  +  sin( (o(p1)-p2) *d)/(o(p1)-p2)  )
-
-	def Hr(qt,p1,p2):
-
-		qt = np.tile(qt,(len(qt),1)).transpose()
-
-		return qt * (Bc(p2) - Bc(p1)) * k**2 * (eps-1) * Bt(p2).conj() * Br(p1).conj() * (  sin( (o(p1)+p2) *d)/(o(p1)+p2)  +  sin( (o(p1)-p2) *d)/(o(p1)-p2)  )
-
-	# Define mesh of p values
-	pmax = p_max*k
-	pres = p_res
-
-	p = np.linspace(1e-9,pmax,pres)
-	
-	# p_nearSingularity = np.linspace(1e-3,2*k,p_res)
-	# p_toMax = np.linspace(2*k,pmax,200)
-	# p = np.hstack((p_nearSingularity[:-1],p_toMax))
-
-	'''
-	2D mesh of p values for performing integration with matrices. Rows correspond to
-	the value of p', columns to p (for H(p',p)).
-
-	Ex. H(p',p)[3,2] chooses the value of H with the 4th value of p' and the 3rd value of p.
-
-	Integrating over p' would then amount to summing over axis=0. This sums the rows of the matrix H, producing 
-	a row vector representing a function of p.
-	'''
-	p2,p1 = np.meshgrid(p,p)
-
-	Zp = Z(p)
-	Zp2 = Z(p2)
-
-
-	# Define initial states for an, qr
-	if initial_a != None:
-		if initial_a.any() == np.nan:
-			# previous iteration failed; start from scratch
-			a = np.zeros(N, dtype=complex)
-		else:
-			a = initial_a
-			if len(a) < N:
-				# pad with zeros
-				for _n in range(N-len(a)):
-					a = np.append(a,0)
-	else:
-		a = np.zeros(N, dtype=complex)
-
-	if initial_d != None:
-		if initial_a.any() == np.nan:
-			# previous iteration failed; start from scratch
-			dd = np.zeros(len(p), dtype=complex)
-		else:
-			dd = initial_d
-	else:
-		dd = np.zeros(len(p), dtype=complex)
-
-
-
-	'''
-	Iteratively define qt,a,qr until the value of a converges to within some threshold.
-	'''
-	repeat = True
-	converged = False
-	delta_prev = delta_2prev = np.inf
-
-	for i in range(imax):
-		print '\nComputing iteration %u of %u' % (i+1,imax)
-
-		if not repeat:
-			break
-
-		# TODO - process the lead terms before looping, since they are independent of the iteration
-		
-		integrand = Ht(dd*Zp,p1,p2)/(p1**2 - p2**2) 	# blows up at p1=p2
-		integrand = smoothMatrix(integrand)				# elminate singular points by using average of nearest neighbors.		
-
-		bb = 1/(2*w*mu*P) * abs(Bc(p))/(B[m]+Bc(p)) * ( \
-			2*B[m]*G(m,p) /Zp \
-			+ np.sum([  (B[m]-B[j])*a[j]*G(j,p) for j in range(N) ], axis=0)/Zp \
-			+ np.trapz(integrand/Zp2, x=p, axis=0) \
-			+ dd * (B[m]-Bc(p)) * pi * Bt(p)*Br(p)*2*np.real(Dr(p))
-			)
-
-		a_prev = a
-		a = [1/(4*w*mu*P) * np.trapz(bb*Zp * (B[j]-Bc(p)) * G(j,p).conj(), x=p) for j in range(N)]; a = np.array(a);
-
-		integrand = Hr(bb*Zp,p2,p1)/(p2**2 - p1**2) # blows up at p1=p2
-		integrand = smoothMatrix(integrand)
-
-		dd = 1/(4*w*mu*P) * abs(Bc(p))/Bc(p) * np.trapz(integrand/Zp2, x=p, axis=0)
-
-		# if i==4:
-		# 	plt.ioff()
-		# 	plt.figure()
-		# 	plt.imshow(abs(integrand))
-		# 	plt.colorbar()
-
-		# 	plt.figure()
-		# 	plt.plot(p/k,bb.real, 'r-')
-		# 	plt.plot(p/k,bb.imag, 'r--')
-		# 	plt.plot(p/k,dd.real, 'b-')
-		# 	plt.plot(p/k,dd.imag, 'b--')
-		# 	plt.show()
-		# 	exit()
-
-		# if returnMode and i == returnItr:
-		# 	# bail and return whatever value we're testing
-		# 	if returnMode == 'qt': return (p,qt)
-		# 	if returnMode == 'qr': return (p,qr)
-		# 	if returnMode == 'a_integral': return (p,qt * (B[j]-Bc(p)) * G(j,p))
-
-
-		# Test for convergence
-		delta = abs(a_prev-a)
-		print 'Delta a:', delta
-		# print 'Delta a:', np.amax(delta)
-		if not np.any(delta > convergence_threshold):
-		 repeat = False
-		 converged = True			
-
-		# if difference in am has been rising for 2 iterations, value is diverging. Bail.
-
-		if np.amax(delta) > delta_prev and delta_prev > delta_2prev: break
-
-		delta_2prev = delta_prev
-		delta_prev = np.amax(delta)
-
-
-	'''
-	Loop completed. Perform Error tests
-	'''
-
-	shouldBeOne = 1/(4*w*mu*P) * np.trapz(bb*Zp*(B[m]+Bc(p))*G(m,p), x=p)
-
-	'''
-	Output results
-	'''
-
-	print "\n--- Outputs ---"
-	if debug:
-		print '|a|:', abs(a)
-		print '<a:', np.angle(a)
-		print 'shouldBeOne:', shouldBeOne
-
-
-	# plt.ioff()
-	# plt.figure()
-	# plt.plot(p/k,np.real(bb*Z(0)),'ro')
-	# plt.plot(p/k,np.imag(bb*Z(0)),'bo')
-	# # plt.xlim(0,3)
-	# plt.show()
-	# exit()
-
-	if converged:
-		return a, bb, dd, np.array(shouldBeOne)
-	else:
-		return a * np.nan, dd*np.nan, bb*np.nan, np.array(np.nan)
-
-def TestHarness():
-
-	# Define Key Simulation Parameters
-	
-	# To convert into the d = h/wl notation for dielectricSlab, use:
-	# kd = d*pi/n
-
-	kd = 0.8
-
-	n = sqrt(20)
-
-	res = [100,200,1000,2000]
-	incident_mode = 0
-	pol='TE'
-	polarity = 'even'
-
-	imax = 200
-	p_max = 10
-
-	method = ReflectionWithHamidsCorrections
-	returnMode = "a_integral"
-	returnItr = 1
-
-	for i,r in enumerate(res):
-		returnVal = method(kd,n,
-										pol=pol,
-										polarity=polarity,
-										incident_mode=incident_mode,
-										p_res=r,
-										imax=imax,
-										p_max=p_max,
-										first_order=False,
-										debug=False,
-										returnMode = returnMode,
-										returnItr = returnItr
-										)
-
-		plt.plot(returnVal[0],returnVal[1])
-
-	plt.legend(res, loc='best')
-	plt.show()
-
-def test_betaMarcuseAtKd():
-
-	'''
-	use for testing mode solver at a specific frequency kd
-	'''
-
-	# constants
-	kd = 2.8
-	n = sqrt(20)
-
-	wl = 10. # Set to 10 to match Gelin values for qt
-	k = 2*pi/wl
-	d = kd/k
-	
-	# w = c*k
-	# eps = n**2
-
-	# Solve slab for all modes
-	N = numModes(n,1,kd)
-	print "Number of Modes from numModes: ", N
-	B = beta_marcuse(n,d,wl=wl,pol='TE',polarity='even',Nmodes=N,plot=True)
-
-def main():
-
-	# Define Key Simulation Parameters
-	
-	# kds = np.array([2.7,2.8])
-	# kds = np.array([0.628])
-	
-	# kds = np.array([0.209,0.418,0.628,0.837,1.04,1.25]) # TE Reference values
-	# kds = np.array([0.314,0.418,0.628,0.837,1.04,1.25]) # TM Reference Values
-
-	# To convert into the d = h/wl notation for dielectricSlab, use:
-	# kd = d*pi/n
-
-	# Note: If kd is too small, BrentQ will fail to converge.
-	kds = np.linspace(1e-2,3,50)
-	# kds = np.linspace(2.8,2.9,100)
-
-	n = sqrt(20)
-
-	slab = Slab(n)
-	slab.setIncidentMode(1)
-
-	slab.polarization 	= 'TE'
-	slab.polarity 			= 'even'
-
-	slab.setMesh()
-	
-	plt.ion()
-	fig, ax = plt.subplots(3,figsize=(4.5,8))
-	ax[2].axhline(1, color='k', ls = ':')
-	plt.show()
-
-	ams = []
-	accuracy = []
-
-	for kdi,kd in enumerate(kds):
-
-		print '\nkd:', kd
-		slab.setFrequencyFromKD(kd)
-		slab.SolveForCoefficients()
-
-		ams.append(slab.a)
-		accuracy.append(np.abs(slab.equation14errorTest()))
-
-		data = stackPoints(ams)
-		data = np.array(data)
-
-		[ax[0].plot(kds[:kdi+1], abs(data[i])        , color=colors[i], marker='o') for i,am in enumerate(data)]
-		[ax[1].plot(kds[:kdi+1], np.angle(data[i])/pi, color=colors[i], marker='o') for i,am in enumerate(data)]
-		ax[0].set_ylim(0,1.2)
-		
-		[ax[2].plot(kds[:kdi+1], accuracy, color='b', marker='o', lw=2) for i,am in enumerate(data)]
-		# ax[2].set_ylim(0.9,1.5)
-
-		fig.canvas.draw()
-
-	plt.ioff()
-	plt.show()
-	output = np.vstack((kds,data,np.array(accuracy, dtype=complex)))
-
-	sname = DATA_PATH + '/Rectangular Resonator/End Facet Reflection Coefs/endFacet_a' + str(incident_mode) + '_' + pol + '_' + polarity + '_' + str(n) + '.txt'
-	np.savetxt(sname,output)
-
 class Slab():
 
 	def __init__(self,n,d=1):
@@ -1092,8 +575,125 @@ class Slab():
 		self.converged = converged
 		return converged
 
+def PrettyPlots():
+
+	n = sqrt(20)
+	
+	incident_mode = 0
+	pol='TE'
+	polarity = 'even'
 
 
+	fname = DATA_PATH + '/Rectangular Resonator/End Facet Reflection Coefs/endFacet_a' + str(incident_mode) + '_' + pol + '_' + polarity + '_' + str(n) + '.txt'
+
+	with open(fname,'r') as f:
+		s = f.read()
+
+	s = s.replace('+-','-')
+	s = s.replace('(','')
+	s = s.replace(')','')
+
+	with open(DATA_PATH + '/endfacet_temp.txt','w') as f:	
+		f.write(s)
+
+
+	data = np.loadtxt(DATA_PATH + '/endfacet_temp.txt', dtype=complex)
+	
+	N = data.shape[0] - 2
+
+	kd = data[0,:]
+	err = data[-1,:]
+
+	idx = np.arange(N) + 1
+
+	fig, ax = plt.subplots(2,sharex=True,figsize=(4.5,7))
+
+	for i in idx:
+		ax[0].plot(kd,  abs(data[i,:])        , color=colors[i-1], lw=2)
+		ax[1].plot(kd, -np.angle(data[i,:])/pi, color=colors[i-1], lw=2)
+
+	ax[1].axhline(0, color='k', ls=':')
+
+	# ax[0].set_xlim(0.6,1.5)
+	ax[0].set_ylim(0,1.1)
+	
+	# ax[1].set_ylim(1/6.,2/3.)
+
+	ax[1].set_xlabel(r'$k_{o}d$')
+
+	ax[0].set_ylabel(r'$|a_{n}|$', fontsize=14)
+	ax[1].set_ylabel(r'$\angle a_{n}$', fontsize=14)
+
+	labels = [r'$a_{%u}$' % (2*j) for j in range(3)]
+	ax[1].legend(labels, loc='lower left')
+
+	ax[0].set_title(pol + ' - ' + r'Source Mode: $a_{%u}$' % 2*incident_mode, fontproperties=prop, fontsize=18)
+
+	plt.tight_layout()
+	plt.show()
+
+def main():
+
+	# Define Key Simulation Parameters
+	
+	# kds = np.array([2.7,2.8])
+	# kds = np.array([0.628])
+	
+	# kds = np.array([0.209,0.418,0.628,0.837,1.04,1.25]) # TE Reference values
+	# kds = np.array([0.314,0.418,0.628,0.837,1.04,1.25]) # TM Reference Values
+
+	# To convert into the d = h/wl notation for dielectricSlab, use:
+	# kd = d*pi/n
+
+	# Note: If kd is too small, BrentQ will fail to converge.
+	kds = np.linspace(1e-2,3,50)
+	# kds = np.linspace(2.8,2.9,100)
+
+	n = sqrt(20)
+
+	slab = Slab(n)
+	slab.setIncidentMode(1)
+
+	slab.polarization 	= 'TE'
+	slab.polarity 			= 'even'
+
+	slab.setMesh()
+	
+	plt.ion()
+	fig, ax = plt.subplots(3,figsize=(4.5,8))
+	ax[2].axhline(1, color='k', ls = ':')
+	plt.show()
+
+	ams = []
+	accuracy = []
+
+	for kdi,kd in enumerate(kds):
+
+		print '\nkd:', kd
+		slab.setFrequencyFromKD(kd)
+		slab.SolveForCoefficients()
+
+		ams.append(slab.a)
+		accuracy.append(np.abs(slab.equation14errorTest()))
+
+		data = stackPoints(ams)
+		data = np.array(data)
+
+		[ax[0].plot(kds[:kdi+1], abs(data[i])        , color=colors[i], marker='o') for i,am in enumerate(data)]
+		[ax[1].plot(kds[:kdi+1], np.angle(data[i])/pi, color=colors[i], marker='o') for i,am in enumerate(data)]
+		ax[0].set_ylim(0,1.2)
+		
+		[ax[2].plot(kds[:kdi+1], accuracy, color='b', marker='o', lw=2) for i,am in enumerate(data)]
+		# ax[2].set_ylim(0.9,1.5)
+
+		fig.canvas.draw()
+
+	plt.ioff()
+	plt.show()
+	output = np.vstack((kds,data,np.array(accuracy, dtype=complex)))
+
+	sname = DATA_PATH + '/Rectangular Resonator/End Facet Reflection Coefs/endFacet_a' + str(incident_mode) + '_' + pol + '_' + polarity + '_' + str(n) + '.txt'
+	np.savetxt(sname,output)
 
 '''
 TODO
@@ -1111,9 +711,3 @@ if __name__ == '__main__':
   ### MAIN FUNCTIONS ###
   main()	
 	# PrettyPlots()
-
-	### TEST FUNCTIONS ###
-  # test_beta_marcuse()
-  # convergence_test_single()
-	# TestHarness()
-	# test_betaMarcuseAtKd()
---
+++ EndFacetTests.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+
+from __future__ import division
+
+from EndFacet import *
+
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+
+import putil
+import sys
+import os
+
+from scipy import sin, cos, exp, tan, arctan, arcsin, arccos
+
+pi = sp.pi
+sqrt = sp.emath.sqrt
+
+colors = ['r','g','b','#FFE21A','c','m']
+
+# Set defaults for plt.imshow()
+plt.rcParams['image.origin'] = 'lower'
+plt.rcParams['image.interpolation'] = 'nearest'
+
+prop = mpl.font_manager.FontProperties(fname='/Library/Fonts/GillSans.ttc')
+
+# Change reporting of numpy objects to lower float precision
+np.set_printoptions(linewidth=150, precision=3)
+
+def test_beta_marcuse():
+
+	target_vals_TE = [0.25781,0.54916,1.21972,1.93825,2.66839,4.13075]
+	target_vals_TM = [0.25207,0.51677,1.12809,1.84210,2.58934,4.08131]
+
+	wl = 1
+
+	n = 1.432
+	k = 2*pi/wl
+
+	kds = np.array([0.25,0.5,1.0,1.5,2.0,3.0])
+	ds = kds/k
+
+	print 'TE'
+	for i,d in enumerate(ds):
+		bTE = beta_marcuse(n,d,pol='TE',wl=wl)[0]
+		print "%.5f, %.5f, %.5f" % (bTE * d, target_vals_TE[i], abs((bTE * d - target_vals_TE[i]) / target_vals_TE[i]))
+
+	print
+	print 'TM'
+
+	for i,d in enumerate(ds):
+		bTM = beta_marcuse(n,d,pol='TM',wl=wl)[0]
+		print "%.5f, %.5f, %.5f" % (bTM * d, target_vals_TM[i], abs((bTM * d - target_vals_TM[i]) / target_vals_TM[i]))
+
+def test_betaMarcuseAtKd():
+
+	'''
+	use for testing mode solver at a specific frequency kd
+	'''
+
+	# constants
+	kd = 2.8
+	n = sqrt(20)
+
+	wl = 10. # Set to 10 to match Gelin values for qt
+	k = 2*pi/wl
+	d = kd/k
+	
+	# w = c*k
+	# eps = n**2
+
+	# Solve slab for all modes
+	N = numModes(n,1,kd)
+	print "Number of Modes from numModes: ", N
+	B = beta_marcuse(n,d,wl=wl,pol='TE',polarity='even',Nmodes=N,plot=True)
+
+def convergence_test_single():
+	'''
+	For a specific value of kd, sweep pmax and pres and plot the result
+	'''
+
+	kd = 2.
+
+	n = sqrt(20)
+
+	res = np.arange(1,15,1) * 1e2
+	p_max = np.arange(5,31,2.5)
+
+	incident_mode = 0
+	pol='TE'
+	polarity = 'even'
+
+	imax = 200
+
+	plt.ion()
+
+	fig, ax = plt.subplots(3,figsize=(10,8),sharex=True,sharey=True)
+	plt.show()
+
+	aos = np.zeros((len(res),len(p_max)),dtype=complex) * complex(np.nan,np.nan)
+	accuracy = np.zeros((len(res),len(p_max)),dtype=complex) * complex(np.nan,np.nan)
+
+	mag = ax[0].imshow(abs(aos), aspect='auto')
+	divmag = make_axes_locatable(ax[0])
+	cmag = divmag.append_axes("right", size="1%", pad=0.05)
+	cbarmag = plt.colorbar(mag,cax=cmag,format='%.2f')
+
+	phase = ax[1].imshow(np.angle(aos)/pi, aspect='auto')
+	divphase = make_axes_locatable(ax[1])
+	cphase = divphase.append_axes("right", size="1%", pad=0.05)
+	cbarphase = plt.colorbar(phase,cax=cphase,format='%.2f')
+
+	error = ax[2].imshow(abs(accuracy), aspect='auto')
+	diverror = make_axes_locatable(ax[2])
+	cerror = diverror.append_axes("right", size="1%", pad=0.05)
+	cbarerror = plt.colorbar(error,cax=cerror,format='%.2f')
+
+	ax[2].set_xlabel(r'Max$\{\rho\}$')
+	ax[1].set_ylabel('res')
+
+
+	for i,res_i in enumerate(res):
+		for j,pmax_j in enumerate(p_max):
+
+			a,dd,bb,acc = SolveForCoefficients(kd,n,
+													pol=pol,
+													polarity=polarity,
+													incident_mode=incident_mode,
+													p_res=res_i,
+													imax=imax,
+													p_max=pmax_j,
+													first_order=False,
+													debug=False
+													)
+
+			try:
+				aos[i,j] = a[0]
+				accuracy[i,j] = acc
+			except TypeError:
+				pass
+
+			# |ao|
+			mag.set_data(abs(aos))
+			mag.set_extent(getExtent(p_max,res))
+			mag.autoscale()
+
+			# phase ao
+			phase.set_data(np.angle(aos)/pi)
+			phase.set_extent(getExtent(p_max,res))
+			phase.autoscale()
+
+			# error
+			error.set_data(abs(accuracy))
+			error.set_extent(getExtent(p_max,res))
+			error.autoscale()
+
+			fig.canvas.draw()
+
+	plt.ioff()
+	plt.show()
+
+
+
+if __name__ == '__main__':
+  test_betaMarcuseAtKd()
